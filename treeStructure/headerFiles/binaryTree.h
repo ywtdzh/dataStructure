e7ebcc096afaeb42307b6c21d9fb4f7f25c4f213
Claim <ywtyx01@126.com>
2016-11-06 22:09:39 +0800

    Add Exercise(10th week)/吼糟糕的代码啊

--- treeStructure/headerFiles/binaryTree.h
+++ treeStructure/headerFiles/binaryTree.h
@@ -7,6 +7,8 @@
 
 #include <iostream>
 #include <vector>
+#include <exception>
+#include <queue>
 
 using namespace std;
 
@@ -31,6 +33,10 @@ protected:
 public:
     binaryTree (const D data, binaryTree *parent = nullptr);
 
+    binaryTree<D> *getLeft ();
+
+    binaryTree<D> *getRight ();
+
     binaryTree<D> *addToLeft (D data);
 
     binaryTree<D> *addToRight (D data);
@@ -51,10 +57,192 @@ public:
 
     friend void recycleBinaryTree<D> (binaryTree<D> *tree);
 
+    const int countWithDegree (const int degree);
+
+    const int getHeight ();
+
+    const int getWidth ();
+
+    const D findMax () const;
+
+    binaryTree<D> *exchangeLeftAndRight ();
+
+    binaryTree<D> *deleteAllLeaves ();
+
+    bool isCompleteBinaryTree ();
+
     binaryTree (const binaryTree &) = delete;
 
     binaryTree &operator= (const binaryTree &)= delete;
 };
+
+
+inline const int max (int a, int b) {
+    return a > b ? a : b;
+}
+
+template<typename D>
+binaryTree<D> *binaryTree<D>::deleteAllLeaves () {
+    queue<binaryTree<D> *> nodeQueue;
+    if (leftTree)
+        nodeQueue.push(leftTree);
+    if (rightTree)
+        nodeQueue.push(rightTree);
+    while (nodeQueue.size()) {
+        queue<binaryTree<D> *> temp;
+        while (nodeQueue.size()) {
+            auto currentNode = nodeQueue.front();
+            nodeQueue.pop();
+            if (currentNode->leftTree && (!currentNode->leftTree->leftTree && !currentNode->leftTree->rightTree)) {
+                delete currentNode->leftTree;
+                currentNode->leftTree = nullptr;
+            }
+            if (currentNode->rightTree && (!currentNode->rightTree->leftTree && !currentNode->rightTree->rightTree)) {
+                delete currentNode->rightTree;
+                currentNode->rightTree = nullptr;
+            }
+            if (currentNode->leftTree)
+                temp.push(currentNode->leftTree);
+            if (currentNode->rightTree)
+                temp.push(currentNode->rightTree);
+        }
+        nodeQueue = temp;
+    }
+    return this;
+}
+
+template<typename D>
+bool binaryTree<D>::isCompleteBinaryTree () {
+    queue<binaryTree<D> *> nodeQueue;
+    bool lackChild = false;
+    if (leftTree)
+        nodeQueue.push(leftTree);
+    if (rightTree)
+        nodeQueue.push(rightTree);
+    while (nodeQueue.size()) {
+        queue<binaryTree<D> *> temp;
+        while (nodeQueue.size()) {
+            auto currentNode = nodeQueue.front();
+            nodeQueue.pop();
+            if (currentNode->leftTree)
+                temp.push(currentNode->leftTree);
+            if (currentNode->rightTree)
+                temp.push(currentNode->rightTree);
+            if (lackChild && (currentNode->leftTree || currentNode->rightTree))
+                return false;
+            if (!lackChild && (!currentNode->leftTree || !currentNode->rightTree))
+                lackChild = true;
+        }
+        nodeQueue = temp;
+    }
+    return true;
+}
+
+template<typename D>
+binaryTree<D> *binaryTree<D>::exchangeLeftAndRight () {
+    auto temp = leftTree;
+    leftTree = rightTree;
+    rightTree = temp;
+    return this;
+}
+
+template<typename D>
+const int binaryTree<D>::getWidth () {
+    queue<binaryTree<D> *> nodeQueue;
+    int width = 0;
+    if (leftTree)
+        nodeQueue.push(leftTree);
+    if (rightTree)
+        nodeQueue.push(rightTree);
+    while (nodeQueue.size()) {
+        width = max<int>(width, nodeQueue.size());
+        queue<binaryTree<D> *> temp;
+        while (nodeQueue.size()) {
+            auto currentNode = nodeQueue.front();
+            nodeQueue.pop();
+            if (currentNode->leftTree)
+                temp.push(currentNode->leftTree);
+            if (currentNode->rightTree)
+                temp.push(currentNode->rightTree);
+        }
+        nodeQueue = temp;
+    }
+    return width;
+}
+
+template<typename D>
+const D binaryTree<D>::findMax () const {
+    queue<binaryTree<D> *> nodeQueue;
+    D maxData = INT32_MIN;
+    if (leftTree)
+        nodeQueue.push(leftTree);
+    if (rightTree)
+        nodeQueue.push(rightTree);
+    while (nodeQueue.size()) {
+        queue<binaryTree<D> *> temp;
+        while (nodeQueue.size()) {
+            auto currentNode = nodeQueue.front();
+            maxData = max<D>(maxData, currentNode->data);
+            nodeQueue.pop();
+            if (currentNode->leftTree)
+                temp.push(currentNode->leftTree);
+            if (currentNode->rightTree)
+                temp.push(currentNode->rightTree);
+        }
+        nodeQueue = temp;
+    }
+    return maxData;
+}
+
+template<typename D>
+const int binaryTree<D>::getHeight () {
+    int height = 0;
+    if (leftTree)
+        height = max(height, leftTree->getHeight() + 1);
+    if (rightTree)
+        height = max(height, rightTree->getHeight() + 1);
+    if (!leftTree && !rightTree)
+        height = 1;
+    return height;
+}
+
+template<typename D>
+const int binaryTree<D>::countWithDegree (const int degree) {
+    if (degree > 2 || degree < 0)
+        throw invalid_argument("Argument fault");
+    switch (degree) {
+        case 0:
+            if (leftTree)
+                if (rightTree)
+                    return leftTree->countWithDegree(0) + rightTree->countWithDegree(0);
+                else
+                    return leftTree->countWithDegree(0);
+            else if (rightTree)
+                return rightTree->countWithDegree(0);
+            else
+                return 1;
+        case 1:
+            if (leftTree)
+                if (rightTree)
+                    return leftTree->countWithDegree(1) + rightTree->countWithDegree(1);
+                else
+                    return leftTree->countWithDegree(1) + 1;
+            else if (rightTree)
+                return rightTree->countWithDegree(1) + 1;
+            else
+                return 0;
+        case 2:
+            if (leftTree)
+                if (rightTree)
+                    return leftTree->countWithDegree(2) + rightTree->countWithDegree(2) + 1;
+                else
+                    return leftTree->countWithDegree(2);
+            else if (rightTree)
+                return rightTree->countWithDegree(2);
+            else
+                return 0;
+    }
+}
 
 template<typename D>
 binaryTree<D>::binaryTree (const D data, binaryTree *parent) {
@@ -82,6 +270,16 @@ binaryTree<D> *binaryTree<D>::addToRight (D data) {
         return this;
     }
 
+}
+
+template<typename D>
+binaryTree<D> *binaryTree<D>::getLeft () {
+    return this->leftTree;
+}
+
+template<typename D>
+binaryTree<D> *binaryTree<D>::getRight () {
+    return this->rightTree;
 }
 
 template<typename D>
